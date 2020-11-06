1fd2201001a49db77839606cf316728a86066ca5
daniel <zietdan@hu-berlin.de>
2020-11-06 13:34:48 +0100

    - implement swap task for i32 with explaining comment

--- assignment1/swap/src/main.rs
+++ assignment1/swap/src/main.rs
@@ -1,3 +1,16 @@
+fn swap(a:&mut i32, b:&mut i32) {
+       /*
+       * Normalerweise würde die folgenden Anweisung ein Verschieben der Ownership von b und a bedeuten, was nicht möglich wäre, da die Funktion nur Referenzen und keinen Besitz über diese Variablen erhält.
+       * Da i32 allerdings Copysemantik anstatt Movesemantik benutzt wird dies hier doch möglich.
+        */
+       let tmp=*b;
+       *b=*a;
+       *a=tmp;
+}
+
 fn main() {
-    println!("Hello, world!");
+       let mut a = 1;
+       let mut b = 2;
+       swap(&mut a, &mut b);
+       println!("a: {}, b: {}", a, b);
 }
