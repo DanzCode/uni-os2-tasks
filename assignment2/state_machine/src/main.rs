69278812cad5c7248de67607158f30e63d2a6aff
daniel <zietdan@hu-berlin.de>
2020-11-17 14:49:40 +0100

    - add basic transition/state implementation

--- assignment2/state_machine/src/main.rs
+++ assignment2/state_machine/src/main.rs
@@ -11,11 +11,37 @@ trait Terminate /* TODO: Trait-Bounds */ {
 }
 
 struct Start;
+impl Start {
+	fn new() -> Start {
+		Start
+	}
+}
+impl State for Start {}
+impl Transition<Loop> for Start {
+	fn transition(self) -> Loop {
+		Loop
+	}
+}
 struct Loop;
+impl State for Loop {}
+impl Transition<Loop> for Loop {
+	fn transition(self) -> Loop {
+		self
+	}
+}
+impl Transition<End> for Loop {
+	fn transition(self) -> End {
+		End
+	}
+}
 struct End;
-
+impl State for End {}
+impl Terminal for End{}
+impl Terminate for End {
+	fn terminate(self) {
+	}
+}
 fn main() {
-	/* TODO: Zustandstransitionen definieren
 	let state = Start::new();
 	// let state: End = state.transition(); // Compilezeitfehler
 	let state: Loop = state.transition();
@@ -24,5 +50,4 @@ fn main() {
 	// let state: Start = state.transition(); // Compilezeitfehler
 	let state: End = state.transition();
 	state.terminate();
-	*/
 }
